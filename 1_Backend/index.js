469a0c5a7de7532cdd410942b9c2224c75d2ab5b
AndresGR00 <angare97@gmail.com>
2024-10-24 23:39:51 +0200

    Configure Vercel

--- 1_Backend/index.js
+++ 1_Backend/index.js
@@ -22,9 +22,9 @@ app.use("*", (req, res, next) => {
   res.status(404).send("<h1> 404 Not found</h1>");
 });
 
-/* const port = 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-}); */
+});
 
 module.exports = app
