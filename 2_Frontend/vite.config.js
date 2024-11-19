b74cbc5b02c2e4b6856311588d56f0ccd7b39f1b
AndresGR00 <angare97@gmail.com>
2024-11-19 20:15:37 +0100

    Modify vite env config

--- 2_Frontend/vite.config.js
+++ 2_Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /* define: {
+  define: {
     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-  }, */
+  },
 });
